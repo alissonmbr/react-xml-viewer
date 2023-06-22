1d23a2e105d076fd702f7c721557958f1f7fd1a2
Alisson Marques Alvarenga <alissonmbr@gmail.com>
2023-06-21 23:43:29 -0300

    chore: build setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import svgr from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import svgr from 'vite-plugin-svgr';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
@@ -15,20 +16,21 @@ export default defineConfig({
     dts({
       insertTypesEntry: true,
     }),
+    tsconfigPaths(),
   ],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "ReactXmlViewer",
-      formats: ["es", "umd"],
-      fileName: (format) => `react-xml-viewer.${format}.js`,
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'ReactXmlViewer',
+      formats: ['es', 'umd'],
+      fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
