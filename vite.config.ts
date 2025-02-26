5a58f0ccf42e4574d1356d9ca981718236e78a5f
Alisson <alissonmbr@gmail.com>
2025-02-25 22:08:12 -0300

    fix: react 19 compatibility (#90)

--- vite.config.ts
+++ vite.config.ts
@@ -27,11 +27,12 @@ export default defineConfig({
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'ReactJsxRuntime',
         },
       },
     },
