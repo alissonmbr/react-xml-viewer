89f0f85623f6acbe9b7eb6618d87258c9b6649a4
Alisson M Alvarenga <alissonmbr@gmail.com>
2025-05-23 23:48:00 -0300

    chore: updating packages

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,22 +5,29 @@ const { mergeConfig } = require('vite');
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@chromatic-com/storybook'
   ],
+
   framework: {
     name: '@storybook/react-vite',
     options: {},
   },
-  docs: {
-    autodocs: 'tag',
-  },
+
+  docs: {},
+
   async viteFinal(config) {
     return mergeConfig(config, {
       plugins: [viteTsconfig()],
     });
   },
+
+  typescript: {
+    reactDocgen: 'react-docgen-typescript'
+  }
 };
 export default config;
